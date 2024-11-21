3fc23b75a2894ccc5b4a08c257416042cbb72ea9
Your NameJanAlSaifi <jan.al.saifi@googlemail.com>
2024-11-21 12:25:26 +0100

    challenges

---
+++ React/react-state-3_adding-animals/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { uid } from "uid";
+import "./styles.css";
+import Form from "./components/Form";
+import List from "./components/List";
+
+const initialAnimals = [
+  {
+    id: "mTBErig",
+    name: "Octopus",
+    emoji: "🐙",
+  },
+  {
+    id: "iMsbXQ1",
+    name: "Crocodile",
+    emoji: "🐊",
+  },
+  {
+    id: "Qjf_K7V",
+    name: "Beaver",
+    emoji: "🦫",
+  },
+];
+
+export default function App() {
+  const [animals, setAnimals] = useState(initialAnimals);
+
+  function handleAddAnimal(newAnimal) {
+    setAnimals([...animals, { ...newAnimal, id: uid() }]);
+    console.log({ ...newAnimal, id: uid() });
+  }
+
+  return (
+    <main className="app">
+      <Form onAddAnimal={handleAddAnimal} />
+      <List animals={animals} />
+    </main>
+  );
+}
